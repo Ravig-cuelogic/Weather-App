7c4334e89267e22ebec81e7cddc23c8e7fbaf40f
Ravi gavade <77093417+Ravig-cuelogic@users.noreply.github.com>
2021-02-16 15:10:18 +0530

    edited dsiplayData methods

--- weather.js
+++ weather.js
@@ -28,21 +28,13 @@ class Weather{
     }
 
     displayData(weatherData){
-
-            if(weatherData.success == undefined)
-            {
-                console.log(`Country - ${weatherData.location.country}`);
-                console.log(`Location - ${weatherData.location.name}`);
-                console.log(`Temperature - ${weatherData.current.temperature}°C`);
-                console.log(`Humidity - ${weatherData.current.humidity}%`);
-                console.log(`Weather Description - ${weatherData.current.weather_descriptions}`);
-                console.log(`Windspeed - ${weatherData.current.wind_speed}km/h`);
-                console.log(`Wind Direction - ${weatherData.current.wind_dir}`);
-            }
-            else
-            {
-                console.log("Please pass a valid name while creating weatherDataect");
-            }
+        console.log(`Country - ${weatherData.location.country}`);
+        console.log(`Location - ${weatherData.location.name}`);
+        console.log(`Temperature - ${weatherData.current.temperature}°C`);
+        console.log(`Humidity - ${weatherData.current.humidity}%`);
+        console.log(`Weather Description - ${weatherData.current.weather_descriptions}`);
+        console.log(`Windspeed - ${weatherData.current.wind_speed}km/h`);
+        console.log(`Wind Direction - ${weatherData.current.wind_dir}`);
     }
 
 }
